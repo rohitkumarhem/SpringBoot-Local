48b8a1146376675748ed3d8ed158cde7d78d7bb2
rohitkumarhem <rohitkumarhem@gmail.com>
2024-05-05 10:01:33 +0530

    Adding final changes

--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -2,11 +2,16 @@ package com.api.book.bootrestbook.entities;
 
 
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,44 +23,48 @@ public class Book {
 	@Column(name="book_id")
     private int id;
     private String title;
-    private String author;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private Author author;
 
     public Book() {
     }
 
-    public Book(int id, String title, String author) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-    }
+	public Book(int id, String title, Author author) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public Author getAuthor() {
+		return author;
+	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
 
-    @Override
-    public String toString() {
-        return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
-    }
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + "]";
+	}
 
+   
 }
---
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/helper/FileUploadHelper.java
@@ -0,0 +1,59 @@
+package com.api.book.bootrestbook.helper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class FileUploadHelper {
+
+	//public final String UPLOAD_DIR="F:\\Git workspace\\SpringBoot-Local\\bootrestbook\\src\\main\\resources\\static\\image";
+	
+	
+	public final String UPLOAD_DIR = new ClassPathResource("static/image").getFile().getAbsolutePath();
+	
+	public FileUploadHelper() throws IOException{
+		
+	}
+	
+	
+	public boolean uploadFile(MultipartFile multipartFile)
+	{
+		boolean f=false;
+		
+		try
+		{
+//			InputStream is =multipartFile.getInputStream();
+//			byte[] data = new byte[is.available()];
+//			is.read(data);
+//			
+//			//write
+//			FileOutputStream fos = new FileOutputStream(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename());
+//			fos.write(data);
+//			
+//			fos.close();
+//			fos.flush();
+			
+		   Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR+File.separator+multipartFile.getOriginalFilename()),
+				   StandardCopyOption.REPLACE_EXISTING);
+			f=true;
+		
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return f;
+		
+	}
+	
+}
